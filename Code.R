52a7f28804148ded24403cf454ebda49812ec67e
Aria Dolatabadian <74664712+Aria-Dolatabadian@users.noreply.github.com>
2023-05-04 14:57:35 +0800

    Update Code.R

--- Code.R
+++ Code.R
@@ -1,19 +1,16 @@
 
 #Place your fastq files in C:\Users\00090473\AppData\Local\R\win-library\4.2\ShortRead\extdata\E-MTAB-1147
-
+#https://compgenomr.github.io/book/filtering-and-trimming-reads.html
 
 library(QuasR)
 library(ShortRead)
 
 
-
-
-
 # obtain a list of fastq file paths
 fastqFiles <- system.file(package="ShortRead",
                           "extdata/E-MTAB-1147",
-                          c("a.fastq",
-                            "b.fastq")
+                          c("Lep14_1_L48BG_TGCCGGTCAG-CCTGATACAA_L001_R1.fastq.gz",
+                            "Lep14_1_L48BG_TGCCGGTCAG-CCTGATACAA_L001_R2.fastq.gz")
 )
 
 # defined processed fastq file names
@@ -32,12 +29,13 @@ preprocessReads(fastqFiles, outfiles,
                 minLength=40)
 
 
-library(ShortRead)
+
+
 
 # obtain a list of fastq file paths
 fastqFile <- system.file(package="ShortRead",
                           "extdata/E-MTAB-1147",
-                          "a.fastq")
+                          "Lep14_1_L48BG_TGCCGGTCAG-CCTGATACAA_L001_R1.fastq.gz")
 
 # read fastq file
 fq = readFastq(fastqFile)
@@ -53,31 +51,8 @@ qcount = rowSums( qPerBase <= 20)
 fq[qcount == 0]
 
 
+
 # write out fastq file with only reads where all 
 # quality scores per base are above 20
 writeFastq(fq[qcount == 0], 
            paste(fastqFile, "Qfiltered", sep="_"))
-
-
-# set up streaming with block size 1000
-# every time we call the yield() function 1000 read portion
-# of the file will be read successively. 
-f <- FastqStreamer(fastqFile,readerBlockSize=1000) 
-
-# we set up a while loop to call yield() function to
-# go through the file
-while(length(fq <- yield(f))) {
-  
-    # remove reads where all quality scores are < 20 
-    # get quality scores per base as a matrix
-    qPerBase = as(quality(fq), "matrix")
-
-    # get number of bases per read that have Q score < 20
-    qcount = rowSums( qPerBase <= 20) 
- 
-    # write fastq file with mode="a", so every new block
-    # is written out to the same file
-    writeFastq(fq[qcount == 0], 
-               paste(fastqFile, "Qfiltered", sep="_"), 
-               mode="a")
-}
