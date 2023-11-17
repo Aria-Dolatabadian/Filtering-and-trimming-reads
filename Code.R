d2977d2f518957d28c6711939dce10e06adb5553
Aria Dolatabadian <74664712+Aria-Dolatabadian@users.noreply.github.com>
2023-11-17 14:54:35 +0800

    Update Code.R

--- Code.R
+++ Code.R
@@ -1,26 +1,17 @@
-
-#Place your fastq files in C:\Users\00090473\AppData\Local\R\win-library\4.2\ShortRead\extdata\E-MTAB-1147
-#https://compgenomr.github.io/book/filtering-and-trimming-reads.html
-
 library(QuasR)
 library(ShortRead)
 
 
-# obtain a list of fastq file paths
-fastqFiles <- system.file(package="ShortRead",
-                          "extdata/E-MTAB-1147",
-                          c("Lep14_1_L48BG_TGCCGGTCAG-CCTGATACAA_L001_R1.fastq.gz",
-                            "Lep14_1_L48BG_TGCCGGTCAG-CCTGATACAA_L001_R2.fastq.gz")
-)
+# obtain a list of fastq file paths from the working directory
+fastqFiles <- list.files(pattern="\\.fastq\\.gz")
 
 # defined processed fastq file names
-outfiles <- paste(tempfile(pattern=c("processed_1_",
-                              "processed_2_")),".fastq",sep="")
+outfiles <- paste(tempfile(pattern=c("processed_1_", "processed_2_")), ".fastq", sep="")
 
 # process fastq files
 # remove reads that have more than 1 N, (nBases)
 # trim 3 bases from the end of the reads (truncateEndBases)
-# Remove ACCCGGGA patern if it occurs at the start (Lpattern)
+# Remove ACCCGGGA pattern if it occurs at the start (Lpattern)
 # remove reads shorter than 40 base-pairs (minLength)
 preprocessReads(fastqFiles, outfiles, 
                 nBases=1,
@@ -28,14 +19,8 @@ preprocessReads(fastqFiles, outfiles,
                 Lpattern="ACCCGGGA",
                 minLength=40)
 
-
-
-
-
 # obtain a list of fastq file paths
-fastqFile <- system.file(package="ShortRead",
-                          "extdata/E-MTAB-1147",
-                          "Lep14_1_L48BG_TGCCGGTCAG-CCTGATACAA_L001_R1.fastq.gz")
+fastqFile <- list.files(pattern="\\.fastq\\.gz")[1]
 
 # read fastq file
 fq = readFastq(fastqFile)
@@ -44,13 +29,10 @@ fq = readFastq(fastqFile)
 qPerBase = as(quality(fq), "matrix")
 
 # get number of bases per read that have quality score below 20
-# we use this
-qcount = rowSums( qPerBase <= 20) 
+qcount = rowSums(qPerBase <= 20) 
 
 # Number of reads where all Phred scores >= 20
 fq[qcount == 0]
-
-
 
 # write out fastq file with only reads where all 
 # quality scores per base are above 20
